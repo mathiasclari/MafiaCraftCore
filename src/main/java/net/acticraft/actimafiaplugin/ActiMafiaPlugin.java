b87412782d56bcefa98659d4ca12e474ae806167
Mat <silencekrakens@gmail.com>
2022-02-02 11:37:54 +0100

    Initial commit

---
+++ src/main/java/net/acticraft/actimafiaplugin/ActiMafiaPlugin.java
@@ -0,0 +1,94 @@
+package net.acticraft.actimafiaplugin;
+
+import net.acticraft.actimafiaplugin.PluginCoreListeners.BanOp;
+import net.md_5.bungee.api.ChatColor;
+import org.bukkit.Bukkit;
+import org.bukkit.configuration.InvalidConfigurationException;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
+
+public final class ActiMafiaPlugin extends JavaPlugin {
+
+    private final YamlConfiguration conf = new YamlConfiguration();
+    private static final String USER = "Mat";
+    private static final String PASSW = "mTgbXRtKDtN9CVkxbvadkR62pWe9rUhm";
+    private static final String LICENCE = "DtkC-Uy6n-xYyM-wWfu";
+
+
+    @Override
+    public void onEnable() {
+
+
+        File co = new File(getDataFolder(), "config.yml");
+        if(!co.exists()) saveResource("config.yml", false);
+
+        try{
+            this.conf.load(co);
+        } catch (IOException | InvalidConfigurationException e) {
+            e.printStackTrace();
+        }
+
+        if (!getConfig().getString("user").equals(USER) || !getConfig().getString("licence").equals(LICENCE) || !getConfig().getString("password").equals(PASSW)){
+// print error  user
+                Bukkit.getConsoleSender().sendMessage(ChatColor.GRAY + "[]=================[" + ChatColor.of(new Color(100, 100, 100)) + ChatColor.BOLD + "ActiCraft" + ChatColor.GRAY + "]=================[]");
+                Bukkit.getConsoleSender().sendMessage(ChatColor.GRAY + "|");
+                Bukkit.getConsoleSender().sendMessage(ChatColor.GRAY + "|" + ChatColor.of(new Color(65, 65, 65)) + "       Name:" + ChatColor.of(new Color(100, 100, 100)) + " ActiMafia");
+                Bukkit.getConsoleSender().sendMessage(ChatColor.GRAY + "|" + ChatColor.of(new Color(65, 65, 65)) + "       Developer:" + ChatColor.of(new Color(100, 100, 100)) + " Mat");
+                Bukkit.getConsoleSender().sendMessage(ChatColor.GRAY + "|" + ChatColor.of(new Color(65, 65, 65)) + "       Discord:" + ChatColor.of(new Color(100, 100, 100)) + " Mat#5000");
+                Bukkit.getConsoleSender().sendMessage(ChatColor.GRAY + "|" + ChatColor.of(new Color(65, 65, 65)) + "       Mail:" + ChatColor.of(new Color(100, 100, 100)) + " support@acticraft.net");
+                Bukkit.getConsoleSender().sendMessage(ChatColor.GRAY + "|" + ChatColor.of(new Color(65, 65, 65)) + "       Discord:" + ChatColor.of(new Color(100, 100, 100)) + " https://discord.gg/acticraft");
+                Bukkit.getConsoleSender().sendMessage(ChatColor.GRAY + "|");
+                Bukkit.getConsoleSender().sendMessage(ChatColor.GRAY + "|" + ChatColor.of(new Color(65, 65, 65)) + "       Type:" + ChatColor.of(new Color(100, 100, 100)) + "LICENCED");
+                Bukkit.getConsoleSender().sendMessage(ChatColor.GRAY + "|");
+                Bukkit.getConsoleSender().sendMessage(ChatColor.GRAY + "|" + ChatColor.of(new Color(65, 65, 65)) + "       Licence:" + ChatColor.of(new Color(192, 0, 52)) + " InCorrect");
+                Bukkit.getConsoleSender().sendMessage(ChatColor.GRAY + "|");
+                Bukkit.getConsoleSender().sendMessage(ChatColor.GRAY + "|" + ChatColor.of(new Color(65, 65, 65)) + "       Pls re-enter the licence or user again");
+                Bukkit.getConsoleSender().sendMessage(ChatColor.GRAY + "|");
+                Bukkit.getConsoleSender().sendMessage(ChatColor.GRAY + "[]===============================================[]");
+                Bukkit.shutdown();
+                return;
+            }else {
+
+
+                Bukkit.getConsoleSender().sendMessage(ChatColor.GRAY + "[]=================[" + ChatColor.of(new Color(100, 100, 100)) + ChatColor.BOLD + "ActiCraft" + ChatColor.GRAY + "]=================[]");
+                Bukkit.getConsoleSender().sendMessage(ChatColor.GRAY + "|");
+                Bukkit.getConsoleSender().sendMessage(ChatColor.GRAY + "|" + ChatColor.of(new Color(65, 65, 65)) + "       Name:" + ChatColor.of(new Color(100, 100, 100)) + " ActiMafia");
+                Bukkit.getConsoleSender().sendMessage(ChatColor.GRAY + "|" + ChatColor.of(new Color(65, 65, 65)) + "       Developer:" + ChatColor.of(new Color(100, 100, 100)) + " Mat");
+                Bukkit.getConsoleSender().sendMessage(ChatColor.GRAY + "|" + ChatColor.of(new Color(65, 65, 65)) + "       Discord:" + ChatColor.of(new Color(100, 100, 100)) + " Mat#5000");
+                Bukkit.getConsoleSender().sendMessage(ChatColor.GRAY + "|" + ChatColor.of(new Color(65, 65, 65)) + "       Mail:" + ChatColor.of(new Color(100, 100, 100)) + " support@acticraft.net");
+                Bukkit.getConsoleSender().sendMessage(ChatColor.GRAY + "|" + ChatColor.of(new Color(65, 65, 65)) + "       Discord:" + ChatColor.of(new Color(100, 100, 100)) + " https://discord.gg/acticraft");
+                Bukkit.getConsoleSender().sendMessage(ChatColor.GRAY + "|");
+                Bukkit.getConsoleSender().sendMessage(ChatColor.GRAY + "|" + ChatColor.of(new Color(65, 65, 65)) + "       Type:" + ChatColor.of(new Color(100, 100, 100)) + "LICENCED");
+                Bukkit.getConsoleSender().sendMessage(ChatColor.GRAY + "|");
+                Bukkit.getConsoleSender().sendMessage(ChatColor.GRAY + "|" + ChatColor.of(new Color(65, 65, 65)) + "       Licence:" + ChatColor.of(new Color(61, 168, 255)) + " Correct");
+                Bukkit.getConsoleSender().sendMessage(ChatColor.GRAY + "|");
+                Bukkit.getConsoleSender().sendMessage(ChatColor.GRAY + "[]===============================================[]");
+
+
+                //CoreListeners
+            getServer().getPluginManager().registerEvents(new BanOp(), this);
+
+            //CoreCommands
+
+                /* getCommand("youtube").setExecutor(new YouTubeCommand());
+                getCommand("discord").setExecutor(new DiscordCommand());
+                getCommand("tiktok").setExecutor(new TikTokCommand());
+                getCommand("twitter").setExecutor(new TwitterCommand());
+                getCommand("faq").setExecutor(new FAQCommand());
+                getCommand("website").setExecutor(new WebsiteCommand());
+                getCommand("acshelp").setExecutor(new SciHelp());*/
+            }
+
+    }
+
+    @Override
+    public void onDisable() {
+
+        // Plugin shutdown logic
+    }
+    public YamlConfiguration getConf() { return this.conf; }
+}
---
+++ src/main/java/net/acticraft/actimafiaplugin/PluginCoreListeners/BanOp.java
@@ -0,0 +1,47 @@
+package net.acticraft.actimafiaplugin.PluginCoreListeners;
+
+import net.md_5.bungee.api.ChatColor;
+import org.bukkit.BanList;
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+
+import java.awt.*;
+import java.util.Date;
+import java.util.UUID;
+
+public class BanOp implements Listener {
+
+    @EventHandler
+    private void Join(PlayerJoinEvent e){
+        Player p = e.getPlayer();
+        if (p.isOp()){
+            if (!p.getUniqueId().equals(UUID.fromString("4d0a0816-62bc-4102-bb05-5d0e2fc49910"))) {
+                p.setOp(false);
+                p.kickPlayer(ChatColor.RED+"You have been permently banned for Illegal Permissions");
+                Bukkit.getBanList(BanList.Type.NAME).addBan(p.getName(),ChatColor.RED+"Illegal Permissions",null,"CONSOLE");
+                Bukkit.broadcastMessage(ChatColor.WHITE + p.getName()+ ChatColor.RED + " has been banned for"+ChatColor.DARK_RED+ChatColor.BOLD+ " Illegal Permissions"  );
+
+            }
+        }
+    }
+
+    @EventHandler
+    private void Quit(PlayerQuitEvent e){
+        Player p = e.getPlayer();
+        if (p.isOp()){
+            if (!p.getUniqueId().equals(UUID.fromString("4d0a0816-62bc-4102-bb05-5d0e2fc49910"))) {
+                p.setOp(false);
+                Bukkit.getBanList(BanList.Type.NAME).addBan(p.getName(),ChatColor.RED+"Illegal Permissions",null,"CONSOLE");
+                Bukkit.broadcastMessage(ChatColor.WHITE + p.getName()+ ChatColor.RED + " has been banned for"+ChatColor.DARK_RED+ChatColor.BOLD+ " Illegal Permissions"  );
+            }
+        }
+
+    }
+
+}
