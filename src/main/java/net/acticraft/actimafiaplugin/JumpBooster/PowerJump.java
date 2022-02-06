9c7cff73bde2122633c8a22e55a1fee72d512e86
Mat <silencekrakens@gmail.com>
2022-02-06 17:19:08 +0100

    Updates 11

---
+++ src/main/java/net/acticraft/actimafiaplugin/JumpBooster/PowerJump.java
@@ -0,0 +1,15 @@
+package net.acticraft.actimafiaplugin.JumpBooster;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.player.PlayerMoveEvent;
+
+public class PowerJump implements Listener {
+    @EventHandler
+    public void onPlayerClick(PlayerInteractEvent e) {
+    Player p = e.getPlayer();
+
+    }
+}
